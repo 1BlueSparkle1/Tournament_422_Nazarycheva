fa9d37fa3d77f83755dc6c751cf5d83dfcc9913c
LERA <LERA@WIN-FL0E7E95RSD>
2024-12-27 15:13:54 +0300

    Creating a connection to the database

---
+++ Tournament_422_Nazarycheva/Tournament_422_Nazarycheva/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Tournament_422_Nazarycheva.Components;
+
+namespace Tournament_422_Nazarycheva
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        TournamentDB_422_NazarychevaEntities db = new TournamentDB_422_NazarychevaEntities();
+    }
+}
